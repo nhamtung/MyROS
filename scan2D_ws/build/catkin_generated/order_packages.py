31e7b98c0a0a6f91abb7a2fa527c165385d670b5
nhamtung <nhamtung125@gmail.com>
2020-03-27 10:25:53 +0700

    rplidar_ws

--- scan2D_ws/build/catkin_generated/order_packages.py
+++ scan2D_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/nhamtung/TungNV/MyROS/scan2D_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/nhamtung/TungNV/MyROS/scan2D_ws/devel;/opt/ros/melodic".split(';') if "/home/nhamtung/TungNV/MyROS/scan2D_ws/devel;/opt/ros/melodic" != "" else []
+underlay_workspaces = "/home/nhamtung/TungNV/MyROS/rplidar_ws/devel;/home/nhamtung/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/nhamtung/TungNV/MyROS/rplidar_ws/devel;/home/nhamtung/catkin_ws/devel;/opt/ros/melodic" != "" else []
